ed7b278c683d196505f68034f23fd7ed26f8bf3b
Martin Walsh <martin.walsh@gmail.com>
2013-10-30 14:03:56 +0000

    Initial Commit

---
+++ NSKeyedArchiverBug/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  NSKeyedArchiverBug
+//
+//  Created by Martin Walsh on 30/10/2013.
+//  Copyright Pedro LTD 2013. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
